a7b9ddf20020b02fcffa33d4c4e12881a2025dc7
mani682 <manichandanamiriyala682@gmail.com>
2022-10-14 10:47:38 +0530

    checking employee attendence

---
+++ uc1_employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewage
+{
+    public static class uc1_employee
+    {
+        public static void ToCheckEmpAttendence()
+        {
+            int Is_Fulltime = 1;
+            Random random = new Random();
+            int emplCheck = random.Next(0, 2);
+            if (emplCheck == 1)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
