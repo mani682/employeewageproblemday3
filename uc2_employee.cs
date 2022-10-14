181491846dab4ba2efa518e362770d4dc31661e4
mani682 <manichandanamiriyala682@gmail.com>
2022-10-14 11:06:27 +0530

    calculate wages

---
+++ uc2_employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewage
+{
+    public static class uc2_employee
+    {
+        public static void ToCheckCalWage()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_PerHr = 20;
+            int empHrs = 0;
+            int empWages = 0;
+            Random random = new Random();
+            int emplCheck = random.Next(2);
+            if (emplCheck == Is_Full_Time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWages = empHrs * Emp_Rate_PerHr;
+            Console.WriteLine("employee daily wages are :" + empWages);
+        }
+    }
+}
